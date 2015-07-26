28616c1cfec195cad52f2b875d52adf158d0ee05
spchal <sreepriya1111@gmail.com>
2015-07-26 06:10:20 +0000

    updates

--- Rtld/audit.c
+++ Rtld/audit.c
@@ -12,5 +12,18 @@ la_objopen(Link_map *lmp, Lmid_t lmid, uintptr_t *cookie)
 {
         if (lmid == LM_ID_BASE)
                 (void) printf("file: %s loaded\n", lmp->l_name);
-        return (0);
+        return (LA_FLG_BINDTO);
 }
+uintptr_t la_symbind32(Elf32_Sym *sym, uint_t ndx,
+        uintptr_t *refcook, uintptr_t *defcook, uint_t *flags) {
+        //if (lmid == LM_ID_BASE)
+         (void) printf("symbol: %s loaded\n", sym->st_name);
+        return(0);
+}
+
+/*uintptr_t la_i86_pltenter(Elf32_Sym *sym, uint_t ndx,
+        uintptr_t *refcook, uintptr_t *defcook,
+        La_i86_regs *regs, uint_t *flags){
+     (void) printf("pltEnter: %s loaded\n", sym->st_name);
+        
+}*/
