441ffc6310aa558f7d0e051dfe68a35b2441ef4b
spchal <sreepriya1111@gmail.com>
2015-08-31 12:17:34 +0000

    ret2libc sample

---
+++ Rtld/sample/ret2libc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+void vul(char *str){
+	char  str2[10];
+	strcpy(str2,str);
+}
+
+int main(int argc,char* argv[]){
+	vul(argv[1]);
+	return 0;
+}
